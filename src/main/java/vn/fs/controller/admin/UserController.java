4ad6d6afc1dba97b62eea2060720fae7a9deba15
phamdocdaoabc <phamdocdao@gmail.com>
2024-10-29 11:37:17 +0700

    Duc add

--- src/main/java/vn/fs/controller/admin/UserController.java
+++ src/main/java/vn/fs/controller/admin/UserController.java
@@ -1,18 +1,24 @@
 package vn.fs.controller.admin;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import vn.fs.entities.User;
 import vn.fs.repository.UserRepository;
+
 
 
 @Controller
@@ -57,4 +63,5 @@ public class UserController{
 	        }
 	        return "redirect:/admin/users";
 	    }
+    	
 }
